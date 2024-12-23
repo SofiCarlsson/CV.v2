aca7074a18f1aafc3d0c850953aee6e2db87164f
Sofi <carlssonsofi99@gmail.com>
2024-12-23 13:32:07 +0100

    stort s

--- Models/User.cs
+++ Models/User.cs
@@ -4,7 +4,7 @@
 	{
 		public int UserId { get; set; }
 		public string Firstname { get; set; }
-		public String Lastname { get; set; }
+		public string Lastname { get; set; }
 		public string Email { get; set; }
 		public string Password { get; set; }
 	}
