3ad79c80e9a04187648741ed7ca51be930005697
Sofi <carlssonsofi99@gmail.com>
2025-01-02 11:39:58 +0100

    Testdata

---
+++ Migrations/20250102103734_test3.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace CV_v2.Migrations
+{
+    /// <inheritdoc />
+    public partial class test3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "CVs",
+                columns: new[] { "CVId", "Competences", "Education", "WorkExperience" },
+                values: new object[,]
+                {
+                    { 1, "SQL", "Örebro Universitet", "Tränare" },
+                    { 2, "C#", "Örebro Universitet", "Arla Foods" },
+                    { 3, "CSS, HTML", "Örebro Universitet", "PostNord" },
+                    { 4, "Java", "Örebro Universitet", "IKEA" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "UserId", "CVID", "Email", "Firstname", "Lastname", "Password" },
+                values: new object[,]
+                {
+                    { 1, 1, "clara.lunak04@gmail.com", "Clara", "Lunak", "svampnisse" },
+                    { 2, 2, "carlssonsofi99@gmail.com", "Sofi", "Carlsson", "juan123" },
+                    { 3, 3, "olivia.cleve@gmail.com", "Olivia", "Cleve", "olivia123" },
+                    { 4, 4, "malin.sandberg@gmail.com", "Malin", "Sandberg", "malin123" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "CVs",
+                keyColumn: "CVId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "CVs",
+                keyColumn: "CVId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "CVs",
+                keyColumn: "CVId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "CVs",
+                keyColumn: "CVId",
+                keyValue: 4);
+        }
+    }
+}
--- Models/UserContext.cs
+++ Models/UserContext.cs
@@ -1,18 +1,88 @@
-﻿using Microsoft.EntityFrameworkCore;	
-using Microsoft.Identity.Client;
-
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace CV_v2.Models
-
-{	
-		public class UserContext : DbContext
-		{
-			public UserContext(DbContextOptions<UserContext> options) : base(options)
+{
+	public class UserContext : DbContext
+	{
+		public UserContext(DbContextOptions<UserContext> options) : base(options)
 		{
 		}
+
 		public DbSet<User> Users { get; set; }
 		public DbSet<Project> Projects { get; set; }
 		public DbSet<CV> CVs { get; set; }
-		}
 
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<CV>().HasData(
+				new CV
+				{
+					CVId = 1,
+					Competences = "SQL",
+					Education = "Örebro Universitet",
+					WorkExperience = "Tränare",
+				},
+				new CV
+				{
+					CVId = 2,
+					Competences = "C#",
+					Education = "Örebro Universitet",
+					WorkExperience = "Arla Foods",
+				},
+				new CV
+				{
+					CVId = 3,
+					Competences = "CSS, HTML",
+					Education = "Örebro Universitet",
+					WorkExperience = "PostNord",
+				},
+				new CV
+				{
+					CVId = 4,
+					Competences = "Java",
+					Education = "Örebro Universitet",
+					WorkExperience = "IKEA",
+				}
+			);
+
+			modelBuilder.Entity<User>().HasData(
+				new User
+				{
+					UserId = 1,
+					Firstname = "Clara",
+					Lastname = "Lunak",
+					Email = "clara.lunak04@gmail.com",
+					Password = "svampnisse",
+					CVID = 1
+				},
+				new User
+				{
+					UserId = 2,
+					Firstname = "Sofi",
+					Lastname = "Carlsson",
+					Email = "carlssonsofi99@gmail.com",
+					Password = "juan123",
+					CVID = 2
+				},
+				new User
+				{
+					UserId = 3,
+					Firstname = "Olivia",
+					Lastname = "Cleve",
+					Email = "olivia.cleve@gmail.com",
+					Password = "olivia123",
+					CVID = 3
+				},
+				new User
+				{
+					UserId = 4,
+					Firstname = "Malin",
+					Lastname = "Sandberg",
+					Email = "malin.sandberg@gmail.com",
+					Password = "malin123",
+					CVID = 4
+				}
+			);
+		}
+	}
 }
