c5fb2191e03def30151ec81c65bf6556536a7e4c
Sofi <carlssonsofi99@gmail.com>
2025-01-09 16:13:38 +0100

    database

---
+++ Migrations/20250109150945_hanna.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CV_v2.Migrations
+{
+    /// <inheritdoc />
+    public partial class hanna : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Messages",
+                columns: table => new
+                {
+                    MessageId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SentTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    last = table.Column<bool>(type: "bit", nullable: true),
+                    FranUserId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    TillUserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Anonym = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Messages", x => x.MessageId);
+                    table.ForeignKey(
+                        name: "FK_Messages_Users_FranUserId",
+                        column: x => x.FranUserId,
+                        principalTable: "Users",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Messages_Users_TillUserId",
+                        column: x => x.TillUserId,
+                        principalTable: "Users",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_FranUserId",
+                table: "Messages",
+                column: "FranUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_TillUserId",
+                table: "Messages",
+                column: "TillUserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Messages");
+        }
+    }
+}
