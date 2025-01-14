1b5c9d5748ed7c3626ddce367069f126c23ebd74
Sofi <carlssonsofi99@gmail.com>
2025-01-14 18:06:01 +0100

    allt förutom lösenord

--- Models/EditProfileViewModel.cs
+++ Models/EditProfileViewModel.cs
@@ -1,14 +1,35 @@
-﻿namespace CV_v2.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CV_v2.Models
 {
     public class EditProfileViewModel
     {
+        [Required(ErrorMessage = "Vänligen ange ett förnamn.")]
         public string Firstname { get; set; }
+
+        [Required(ErrorMessage = "Vänligen ange ett efternamn.")]
         public string Lastname { get; set; }
+
+        [Required(ErrorMessage = "Vänligen ange en e-postadress.")]
+        [EmailAddress(ErrorMessage = "Vänligen ange en giltig e-postadress.")]
         public string Email { get; set; }
+
         public bool IsProfilePrivate { get; set; }
+
+        [Required(ErrorMessage = "Vänligen ange en adress.")]
         public string Address { get; set; }
+        
+        [DataType(DataType.Password)]
+        [Display(Name = "Gammalt lösenord")]
         public string OldPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Nytt lösenord")]
         public string NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("NewPassword", ErrorMessage = "Lösenorden matchar inte.")]
+        [Display(Name = "Bekräfta nytt lösenord")]
         public string ConfirmNewPassword { get; set; }
     }
 }
